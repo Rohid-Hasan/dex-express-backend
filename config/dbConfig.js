ca0658cfa9b68652c04ad133329fe9e289f94bf2
Rohid-Hasan <rohedhasan3499@gmail.com>
2022-10-10 15:57:45 +0600

    git server database connected

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("dex", "postgres", "Protected00", {
+/* module.exports = new Sequelize("dex", "postgres", "Protected00", {
   host: "localhost",
   port: 5432,
   dialect: "postgres",
@@ -10,9 +10,9 @@ module.exports = new Sequelize("dex", "postgres", "Protected00", {
     acquire: 30000,
     idle: 10000,
   },
-});
+}); */
 
-/* module.exports = new Sequelize("hrms1", "raghav", "Chachi#499", {
+module.exports = new Sequelize("hrms1", "raghav", "Chachi#499", {
   host: "167.172.151.136",
   port: 5432,
   dialect: "postgres",
@@ -22,4 +22,4 @@ module.exports = new Sequelize("dex", "postgres", "Protected00", {
     acquire: 30000,
     idle: 10000,
   },
-}); */
+});
